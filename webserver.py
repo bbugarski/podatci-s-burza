8b2bef20d346bba77ae12682672f9389435a95e2
b.bugarski <b.bugarsk@levi9.com>
2016-07-13 21:50:50 +0200

    annað fremja - table added and populated

--- webserver.py
+++ webserver.py
@@ -5,22 +5,60 @@ import time
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello Worddddld!"
 
+
 @app.route("/table")
 def table():
-    return "...table html..."
+    tabela = ''
+    for e in data.prices:
+        tabela+='''
+        <tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+        </tr>
+    ''' % e
+
+    stil = '''
+    <style>
+        th,tr, td {
+            border: 1px solid black;
+            border-color: black;
+        }
+    </style>
+
+    <html>
+        <head>
+	    <meta http-equiv="refresh" content="5">
+	    </head>
+        <body>
+        <table style="width:100%%";>
+          <tr>
+            <th>Date</th>
+            <th>Symbol</th>
+            <th>Price</th>
+          </tr>
+          %s
+        </body>
+    </html>
+            '''
+    return stil % tabela
+
 
 @app.route("/graph")
 def graph():
     return "...graph html..."
 
+
 @app.route("/getdata")
 def getdata():
     data.get_data()
     return "OK\n"
+
 
 @app.route("/data")
 def returndata():
@@ -36,10 +74,12 @@ def returndata():
     '''
     return s % data.prices
 
+
 def poller():
     while True:
         data.get_data()
         time.sleep(10)
+
 
 t = threading.Thread(target=poller)
 t.setDaemon(True)
@@ -47,4 +87,3 @@ t.start()
 
 if __name__ == "__main__":
     app.run()
-
